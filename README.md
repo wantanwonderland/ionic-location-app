e5fb13172414c51a08a34d39eb69700bb9fb59a2
wantanwonderland <shafuan1015@gmail.com>
2022-07-08 22:13:32 +0800

    add google map view

---
+++ README.md
@@ -0,0 +1,12 @@
+## Description
+This is demo on how to use Google Maps Native SDK with Ionic Capacitor
+
+## Package
+
+```bash
+npm install @capacitor/google-maps
+npm install @capacitor/geolocation
+```
+
+## Google Maps API Key
+To use the Google Maps SDK on any platform, API keys associated with an account with billing enabled are required. These can be obtained from the Google Cloud Console. This is required for all three platforms, Android, iOS, and Javascript. Additional information about obtaining these API keys can be found in the Google Maps documentation (https://developers.google.com/maps/documentation/android-sdk/overview) for each platform.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap, Marker } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  @ViewChild('map')mapRef: ElementRef;
+  map: GoogleMap;
+
   constructor() {}
 
+  ionViewDidEnter() {
+    this.creatMap();
+  }
+
+  async creatMap() {
+    this.map = await GoogleMap.create({
+      id: 'my-map',
+      apiKey: environment.mapsKey,
+      element: this.mapRef.nativeElement,
+      forceCreate: true,
+      config: {
+        center: {
+          lat: 2.9213,
+          lng: 101.6559,
+        },
+        zoom: 15,
+        devicePixelRatio: window.devicePixelRatio,
+      }
+    });
+
+    this.addMarkers();
+  }
+
+  async addMarkers() {
+    const marker: Marker =
+      {
+        coordinate: {
+          lat: 2.9213,
+          lng: 101.6559,
+        },
+        draggable: true
+      };
+
+
+    await this.map.addMarker(marker);
+  }
 }
